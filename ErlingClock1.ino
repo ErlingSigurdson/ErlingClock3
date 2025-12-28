fa0c5a5947a10ccd01a643f6b8d7d92e61fbcee8
Erling Sigurdson <efimov-d-v@yandex.ru>
2025-12-29 01:08:18 +0300

    Well, now it works. But I don't call it version 2.0 yet.

--- ErlingClock1.ino
+++ ErlingClock1.ino
@@ -14,12 +14,15 @@
 
 /*--- Includes ---*/
 
+// Display driver.
 #include <Drv7SegQ595.h>
 #include <SegMap595.h>
 
+// Buttons.
 #include <GyverIO.h>
 #include <uButton.h>
 
+// RTC interfacing.
 #include <Wire.h>
 #include <DS3231.h>
 
@@ -46,16 +49,6 @@
 #define DISPLAY_COMMON_PIN SegMap595CommonCathode
 
 
-/*--- Timing ---*/
-
-// In milliseconds.
-#define AUTONOMOUS_COUNTER_INTERVAL 1000
-#define I2C_READ_INTERVAL           10000
-
-#define MAX_COUNT_MINUTES 60
-#define MAX_COUNT_SECONDS 60
-
-
 /*--- Buttons ---*/
 
 #define BTN_1_PIN 10
@@ -63,32 +56,42 @@
 #define BTN_3_PIN 11
 
 
+/*--- Counters and timing ---*/
+
+#define INTERVAL                     1000  // In milliseconds.
+#define I2C_READ_INTERVAL_MULTIPLIER 10
+
+#define MAX_COUNT_HOURS   24
+#define MAX_COUNT_MINUTES 60
+#define MAX_COUNT_SECONDS 60
+
+
 /*--- UART ---*/
 
-#define SERIAL_OUTPUT  // Comment out or delete to suppress the output of current timer values via UART.
+#define SERIAL_OUTPUT_ENABLED  // Comment out or delete to suppress the output of current timer values via UART.
 #define BAUD_RATE 115200
 
 
 /****************** DATA TYPES ******************/
 
 struct current_time_t {
-    uint8_t raw_hours;
-    uint8_t raw_minutes;
-    uint8_t raw_seconds;
+    uint32_t raw_hours;
+    uint32_t raw_minutes;
+    uint32_t raw_seconds;
 
     struct hours_t {
-        uint8_t tens;
-        uint8_t ones;
+        size_t tens;  // size_t is used because this and following values are used as indices.
+        size_t ones;
     }; 
 
     struct minutes_t {
-        uint8_t tens;
-        uint8_t ones;
+        size_t tens;
+        size_t ones;
     };
 
     struct seconds_t {
-        uint8_t tens;
-        uint8_t ones;
+        size_t tens;
+        size_t ones;
     };
     
     hours_t   hours;
@@ -98,8 +101,6 @@ struct current_time_t {
 
 
 /*************** GLOBAL VARIABLES ***************/
-
-DS3231 RTC;
 
 uButton btn_1(BTN_1_PIN);
 uButton btn_2(BTN_2_PIN);
@@ -111,23 +112,21 @@ uButton btn_3(BTN_3_PIN);
 /*--- Multiplexing-friendly I/O wrappers ---*/
 
 namespace mp_safe_io {
-    // I2C interface.
-    void copy_rtc_time(DS3231& RTC, current_time_t& CurrentTime);
-    void set_rtc_time(DS3231& RTC, current_time_t& CurrentTime);
+    // I2C.
+    void read_rtc_time(DS3231& RTC, current_time_t& CurrentTime);
+    void write_rtc_time(DS3231& RTC, current_time_t& CurrentTime);
 
-    // I2C helpers.
-    void read_time(DS3231& RTC, current_time_t& CurrentTime);
-
-    // UART interface.
+    // UART.
     void serial_print(const char* msg);
-    void serial_print(int8_t* val);
+    void serial_print(size_t val);
+    void serial_print(uint32_t val);
 }
 
 
 /*--- Misc ---*/
 
-void decompose_time(current_time_t& CurrentTime);
-void time_setting_mode(bool& time_setting_mode_flag, DS3231& RTC, current_time_t& CurrentTime);
+void decompose_rtc_time(current_time_t& CurrentTime);
+void time_setting_mode(DS3231& RTC, current_time_t& CurrentTime, bool& time_setting_mode_flag);
 
 
 /******************* FUNCTIONS ******************/
@@ -138,7 +137,7 @@ void setup()
 {
     /*--- Interface initialization ---*/
 
-    #ifdef SERIAL_OUTPUT
+    #ifdef SERIAL_OUTPUT_ENABLED
     Serial.begin(BAUD_RATE);
     #endif
     
@@ -183,6 +182,7 @@ void setup()
 
 void loop()
 {
+    DS3231 RTC;
     static current_time_t CurrentTime = {};  // Initialize with all-zero values.
 
 
@@ -192,63 +192,88 @@ void loop()
     uint32_t current_millis = millis();
     static uint32_t previous_millis = current_millis;
 
+    static uint32_t updates = 0;
+
+    if (CurrentTime.raw_seconds >= MAX_COUNT_SECONDS) {
+        CurrentTime.raw_minutes++;
+        CurrentTime.raw_seconds = 0;
+    }
+
+    if (CurrentTime.raw_minutes >= MAX_COUNT_MINUTES) {
+        CurrentTime.raw_hours++;
+        CurrentTime.raw_minutes = 0;
+    }
+
+    if (CurrentTime.raw_hours >= MAX_COUNT_HOURS) {
+        CurrentTime.raw_hours = 0;
+    }
+
     // Value update trigger.
-    static bool update_due = true;
+    static bool update_output_due = true;
+    static bool update_i2c_due = true;
 
 
-    /*--- Demo output ---*/
+    /*--- Time update and output ---*/
 
-    if (update_due) {
-        get_time(RTC, CurrentTime);
+    if (update_i2c_due) {
+        mp_safe_io::read_rtc_time(RTC, CurrentTime);
+        update_i2c_due = false;
+    }
 
-        uint8_t seg_byte_minutes_tens = SegMap595.get_mapped_byte(static_cast<uint32_t>(CurrentTime.minutes.tens));
-        uint8_t seg_byte_minutes_ones = SegMap595.get_mapped_byte(static_cast<uint32_t>(CurrentTime.minutes.ones));
-        uint8_t seg_byte_seconds_tens = SegMap595.get_mapped_byte(static_cast<uint32_t>(CurrentTime.seconds.tens));
-        uint8_t seg_byte_seconds_ones = SegMap595.get_mapped_byte(static_cast<uint32_t>(CurrentTime.seconds.ones));
+    if (update_output_due) {
+        decompose_rtc_time(CurrentTime);
+
+        uint8_t seg_byte_pos_1 = SegMap595.get_mapped_byte(CurrentTime.hours.tens);
+        uint8_t seg_byte_pos_2 = SegMap595.get_mapped_byte(CurrentTime.hours.ones);
+        uint8_t seg_byte_pos_3 = SegMap595.get_mapped_byte(CurrentTime.minutes.tens);
+        uint8_t seg_byte_pos_4 = SegMap595.get_mapped_byte(CurrentTime.minutes.ones);
 
         // Dot-segment blink.
         if (CurrentTime.raw_seconds % 2) {
-            /* static keyword is only suitable if you're not planning subsequent
-             * init() calls that can change the actual dot bit position.
-             */
             static int32_t dot_bit_pos = SegMap595.get_dot_bit_pos();
             if (dot_bit_pos >= 0) {
                 uint8_t mask = static_cast<uint8_t>(1u << dot_bit_pos);
-                seg_byte_minutes_ones ^= mask;
+                seg_byte_pos_2 ^= mask;
             }
         }
 
-        Drv7Seg.set_glyph_to_pos(seg_byte_minutes_tens, Drv7SegPos1);
-        Drv7Seg.set_glyph_to_pos(seg_byte_minutes_ones, Drv7SegPos2);
-        Drv7Seg.set_glyph_to_pos(seg_byte_seconds_tens, Drv7SegPos3);
-        Drv7Seg.set_glyph_to_pos(seg_byte_seconds_ones, Drv7SegPos4);
+        Drv7Seg.set_glyph_to_pos(seg_byte_pos_1, Drv7SegPos1);
+        Drv7Seg.set_glyph_to_pos(seg_byte_pos_2, Drv7SegPos2);
+        Drv7Seg.set_glyph_to_pos(seg_byte_pos_3, Drv7SegPos3);
+        Drv7Seg.set_glyph_to_pos(seg_byte_pos_4, Drv7SegPos4);
 
-        #ifdef SERIAL_OUTPUT
-            Serial.print("Timer values (minutes and seconds): ");
-            Drv7Seg.output_all();
-            Serial.print(CurrentTime.raw_minutes / 10);
-            Drv7Seg.output_all();
-            Serial.print(CurrentTime.raw_minutes % 10);
-            Drv7Seg.output_all();
-            Serial.print(":");
-            Drv7Seg.output_all();
-            Serial.print(CurrentTime.raw_seconds / 10);
-            Drv7Seg.output_all();
-            Serial.println(CurrentTime.raw_seconds % 10);
+        #ifdef SERIAL_OUTPUT_ENABLED
+            mp_safe_io::serial_print("Timer values (minutes and seconds): ");
+            mp_safe_io::serial_print(CurrentTime.hours.tens);
+            mp_safe_io::serial_print(CurrentTime.hours.ones);
+            mp_safe_io::serial_print(":");
+            mp_safe_io::serial_print(CurrentTime.minutes.tens);
+            mp_safe_io::serial_print(CurrentTime.minutes.ones);
+            mp_safe_io::serial_print(":");
+            mp_safe_io::serial_print(CurrentTime.seconds.tens);
+            mp_safe_io::serial_print(CurrentTime.seconds.ones);
+            mp_safe_io::serial_print("\r\n");
         #endif
 
-        update_due = false;
+        update_output_due = false;
     }
 
-    // Basic call spot (it's not the only one).
+    // Basic output call spot (it's not the only one).
     Drv7Seg.output_all();
 
 
     /*--- Counter and value update trigger, continued ---*/
 
     if (current_millis - previous_millis >= INTERVAL) {
-        update_due = true;
+        CurrentTime.raw_seconds++;
+        update_output_due = true;
+        ++updates;
         previous_millis = current_millis;
+    }
+
+    if (updates >= I2C_READ_INTERVAL_MULTIPLIER) {
+        update_i2c_due = true;
+        updates = 0;
     }
 
 
@@ -263,32 +288,50 @@ void loop()
     }
 
     if (time_setting_mode_flag) {
-        time_setting_mode(time_setting_mode_flag, RTC, CurrentTime);
+        time_setting_mode(RTC, CurrentTime, time_setting_mode_flag);
     }
 }
 
 
 /*--- Extra functions ---*/
 
-void mp_safe_io::copy_time(DS3231& RTC, current_time_t& CurrentTime)
+void mp_safe_io::read_rtc_time(DS3231& RTC, current_time_t& CurrentTime)
 {
-    read_time(RTC, CurrentTime);
-    decompose_time(CurrentTime);
+    static bool h12_flag = false;
+    static bool pm_time_flag = false;
+
+    Drv7Seg.output_all();
+    CurrentTime.raw_hours   = static_cast<uint32_t>(RTC.getHour(h12_flag, pm_time_flag));
+    Drv7Seg.output_all();
+    CurrentTime.raw_minutes = static_cast<uint32_t>(RTC.getMinute());
+    Drv7Seg.output_all();
+    CurrentTime.raw_seconds = static_cast<uint32_t>(RTC.getSecond());
+    Drv7Seg.output_all();
 }
 
-void read_time(DS3231& RTC, current_time_t& CurrentTime)
+void mp_safe_io::write_rtc_time(DS3231& RTC, current_time_t& CurrentTime)
 {
-    constexpr bool h12_flag = false;
-    constexpt bool pm_time_flag = false;
-
-    CurrentTime.raw_hours   = RTC.getHour(h12_flag, pm_time_flag);
-    Drv7Seg.output_all();
-    CurrentTime.raw_minutes = RTC.getMinute();
-    Drv7Seg.output_all();
-    CurrentTime.raw_seconds = RTC.getSecond();
+    RTC.setHour(CurrentTime.raw_hours);
+    RTC.setMinute(CurrentTime.raw_minutes);
+    RTC.setSecond(CurrentTime.raw_seconds);
 }
 
-void decompose_time(current_time_t& CurrentTime)
+void mp_safe_io::serial_print(const char *msg)
+{
+    Serial.print(msg);
+}
+
+void mp_safe_io::serial_print(size_t val)
+{
+    Serial.print(val);
+}
+
+void mp_safe_io::serial_print(uint32_t val)
+{
+    serial_print(static_cast<size_t>(val));
+}
+
+void decompose_rtc_time(current_time_t& CurrentTime)
 {
     CurrentTime.hours.tens = CurrentTime.raw_hours / 10;
     CurrentTime.hours.ones = CurrentTime.raw_hours % 10;
@@ -300,23 +343,60 @@ void decompose_time(current_time_t& CurrentTime)
     CurrentTime.seconds.ones = CurrentTime.raw_seconds % 10;
 }
 
-void set_time(DS3231& RTC, current_time_t& CurrentTime)
+void time_setting_mode(DS3231& RTC, current_time_t& CurrentTime, bool& time_setting_mode_flag)
 {
-    RTC.setHour(CurrentTime.raw_hours);
-    RTC.setMinute(CurrentTime.raw_minutes);
-    RTC.setSecond(CurrentTime.raw_seconds);
-}
+    CurrentTime = {};  // Assign all-zero values.
+    bool update_glyphs_due = true;
 
-void time_setting_mode(bool& time_setting_mode_flag, DS3231& RTC, current_time_t& CurrentTime)
-{
-    // TODO
     while (time_setting_mode_flag) {
-        /*
+        Drv7Seg.output_all();
+
+
+        /*--- Buttons ---*/
+
         if (btn_1.tick()) {
             if (btn_1.press()) {
+                mp_safe_io::write_rtc_time(RTC, CurrentTime);
                 time_setting_mode_flag = false;
             }
         }
-        */
+
+        if (btn_2.tick()) {
+            if (btn_2.press() || btn_2.step()) {
+                CurrentTime.raw_hours++;
+                update_glyphs_due = true;
+            }
+        }
+
+        if (btn_3.tick()) {
+            if (btn_3.press() || btn_3.step()) {
+                CurrentTime.raw_minutes++;
+                update_glyphs_due = true;
+            }
+        }    
+
+        if (CurrentTime.raw_minutes >= MAX_COUNT_MINUTES) {
+            CurrentTime.raw_minutes = 0;
+        }
+
+        if (CurrentTime.raw_hours >= MAX_COUNT_HOURS) {
+            CurrentTime.raw_hours = 0;
+        }
+
+        if (update_glyphs_due) {
+                decompose_rtc_time(CurrentTime);
+
+                uint8_t seg_byte_pos_1 = SegMap595.get_mapped_byte(CurrentTime.hours.tens);
+                uint8_t seg_byte_pos_2 = SegMap595.get_mapped_byte(CurrentTime.hours.ones);
+                uint8_t seg_byte_pos_3 = SegMap595.get_mapped_byte(CurrentTime.minutes.tens);
+                uint8_t seg_byte_pos_4 = SegMap595.get_mapped_byte(CurrentTime.minutes.ones);
+
+                Drv7Seg.set_glyph_to_pos(seg_byte_pos_1, Drv7SegPos1);
+                Drv7Seg.set_glyph_to_pos(seg_byte_pos_2, Drv7SegPos2);
+                Drv7Seg.set_glyph_to_pos(seg_byte_pos_3, Drv7SegPos3);
+                Drv7Seg.set_glyph_to_pos(seg_byte_pos_4, Drv7SegPos4);
+
+                update_glyphs_due = false;
+        }
     }
 }
